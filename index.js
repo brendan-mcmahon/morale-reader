d45d6d9786d702fd98b22cb57fef47e334c951a0
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-04-10 09:56:16 -0400

    The whole dang thing

---
+++ index.js
@@ -0,0 +1,41 @@
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+  const guid = event.pathParameters.guid;
+  const value = parseInt(event.pathParameters.value);
+
+  try {
+    await updateRecordValue(guid, value);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify("Record updated successfully"),
+    };
+  } catch (error) {
+    console.error("Error updating record", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Error updating record"),
+    };
+  }
+};
+
+const updateRecordValue = async (guid, value) => {
+  const params = {
+    TableName: "morale",
+    Key: {
+      id: guid,
+    },
+    UpdateExpression: "SET #value = :value",
+    ExpressionAttributeNames: {
+      "#value": "value",
+    },
+    ExpressionAttributeValues: {
+      ":value": value,
+    },
+  };
+
+  await dynamodb.update(params).promise();
+};
